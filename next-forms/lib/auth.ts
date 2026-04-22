372c2e07b170118f1083c4aa00191af01bb0de8c
namiyama <namiyama814@gmail.com>
2026-04-22 17:50:38 +0900

    ユーザー情報の保存処理を実装

--- next-forms/lib/auth.ts
+++ next-forms/lib/auth.ts
@@ -3,6 +3,9 @@ import Google from "next-auth/providers/google";
 import { UpstashRedisAdapter } from "@auth/upstash-redis-adapter";
 import { Redis } from "@upstash/redis";
 
+import prisma from "./prisma";
+import { use } from "react";
+
 const redis = new Redis({
   url: process.env.UPSTASH_REDIS_URL!,
   token: process.env.UPSTASH_REDIS_TOKEN!
@@ -11,6 +14,50 @@ const redis = new Redis({
 const authConfig: NextAuthConfig = {
   providers: [Google],
   adapter: UpstashRedisAdapter(redis),
+
+  callbacks: {
+    async signIn({ user }) {
+      if (!user.id || !user.name) {
+        return false;
+      }
+      const data = {
+        authjsUserId: user.id,
+        username: user.name,
+      };
+
+      try {
+        await prisma.user.upsert({
+          where: { authjsUserId: user.id },
+          update: data,
+          create: data,
+        });
+        return true;
+      } catch (error) {
+        console.error('callbacks.signIn Database error', error);
+        return false;
+      }
+    },
+
+    async session({ session, user }) {
+      if (!session.user || !user?.id) return session;
+
+      try {
+        const appUser = await prisma.user.findUnique({
+          where: { authjsUserId: user.id },
+          select: { userId: true },
+        });
+
+        if (appUser) {
+          session.user.id = appUser.userId;
+        } else {
+          delete (session.user as { id?: string }).id;
+        }
+      } catch (error) {
+        console.error('callbacks.session Database error:', error)
+      }
+      return session;
+    },
+  },
 }
 
 export const { handlers, signIn, signOut, auth } = NextAuth(authConfig);
